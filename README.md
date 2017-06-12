eac2fa91f3c257c535ddb56191821967501ff8d6
Vitaly Khorin <khorin.v@gmail.com>
2017-06-13 00:01:22 +0600

    Реализована задача, подготовлено описание

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+The script finds all files duplicates in specified directory and also in child directories. 
+It compare name and size of files. 
+ 
+# Quickstart
+
+You will need python 3.5 interpreter. Then you can launch 
+script in terminal or import functions in your code. 
+
+launch on Linux, Python 3.5:
+
+```#!bash
+
+$ python3 duplicates.py <path to directory for check>
+#output example
+Directory contain duplicates:
+./.git/logs/HEAD
+./.git/logs/refs/remotes/origin/HEAD
+```
+
+Example of using functions in code:
+
+```
+import duplicates
+
+files = all_files(your_path)
+print(find_duplicates(files))
+
+```
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,58 @@
+from os import listdir
+from os.path import isdir, getsize
+from sys import argv
+
+
+def all_files(dir_path):
+    dir_content = listdir(dir_path)
+    files = []
+
+    for dir_item in dir_content:
+        file_path = dir_path + "/" + dir_item
+        if not isdir(file_path):
+            file_inf = {"name": dir_item, "size": getsize(file_path), "path": dir_path}
+            files.append(file_inf)
+        else:
+            child_dir_files = all_files(file_path)
+            files.extend(child_dir_files)
+
+    return files
+
+
+def find_duplicates(files):
+    uniq_items = []
+    for file in files:
+        short_file_inf = {"name": file["name"], "size": file["size"]}
+        if short_file_inf not in uniq_items: uniq_items.append(short_file_inf)
+
+    duplicates = []
+    for item in uniq_items:
+        sub_duplicates = []
+        for file in files:
+            if file["name"] == item["name"] and file["size"] == item["size"]:
+                file_path = file["path"] + "/" + file["name"]
+                sub_duplicates.append(file_path)
+
+        if sub_duplicates.__len__() > 1:
+            duplicates.extend(sub_duplicates)
+
+    return duplicates
+
 
 if __name__ == '__main__':
-    pass
+    try:
+        files = all_files(argv[1])
+        duplicates = find_duplicates(files)
+        if duplicates.__len__() == 0:
+            print("Duplicates not found")
+        else:
+            print("Directory contain duplicates:")
+            for item in duplicates: print(item)
+    except IndexError:
+        print("Directory not specified!")
+    except FileNotFoundError:
+        print("Directory not found!")
+    except NotADirectoryError:
+        print("It is not a directory!")
+
+
